2deef17116fe5b3a2d7321c26103cd992749856f
EdrikCamacho <edrikeduardo04@gmail.com>
2025-12-04 01:41:59 -0700

    No cambie nada solo prueba

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('BuildProject');
+  protected readonly title = signal('BuildojoProject');
 }
